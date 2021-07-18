ce731baea4c3586274702aba4499509066ae3a62
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2021-07-18 20:25:50 +0530

    New products now get stored in mongodb

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
-const routes = require('./src/routes/routes');
 const nunjucks = require('nunjucks');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const routes = require('./src/routes/routes');
 
 const port = 3000;
+
+const app = express();
 
 nunjucks.configure('src/views', {
     autoescape: true,
@@ -12,6 +16,18 @@ nunjucks.configure('src/views', {
 
 app.use(routes);
 
-app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
-});
+(async () => {
+    try {
+        console.log('Connecting to database');
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected!');
+        app.listen(port, () => {
+            console.log(`listening at http://localhost:${port}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+})();
--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,10 +1,12 @@
 const Url = require('url-parse');
 const puppeteer = require('puppeteer');
 
+const Product = require('../models/product');
+
 module.exports.addProduct = async (req, res) => {
     const url = new Url(req.query.url);
-    const shortUrl = `${url.origin}/dp/${url.pathname.split('/').pop()}`;
-    console.log(shortUrl);
+    const asin = url.pathname.split('/').pop();
+    const shortUrl = `${url.origin}/dp/${asin}`;
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(shortUrl);
@@ -14,7 +16,19 @@ module.exports.addProduct = async (req, res) => {
     } catch (err) {
         price = await page.$eval('#priceblock_dealprice', (el) => el.innerHTML);
     }
-    console.log(price);
+    const currency = price.substring(0, 1);
+    price = Number.parseFloat(price.substr(1).replace(',', ''));
     await browser.close();
+    const product = new Product({
+        asin: asin,
+        url: shortUrl,
+        currency: currency,
+        priceLog: [{
+            timestamp: Date.now(),
+            price: price
+        }]
+    });
+    await product.save();
+    console.log('product added');
     res.send('product added');
 };
