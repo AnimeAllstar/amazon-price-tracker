caea6b3a9fd8d792c9b5c75279890681cfff5ce1
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2021-08-01 22:41:25 +0530

    Fixed bug caused by empty query string

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -3,6 +3,8 @@ const User = require('../models/user');
 
 module.exports.authUser = async (req, res) => {
   mongoose.set('useFindAndModify', false);
-  const data = await User.findOneAndUpdate({}, { name: req.body.name, email: req.body.email }, { upsert: true });
+  const query = { email: req.body.email };
+  const update = { name: req.body.name, email: req.body.email };
+  const data = await User.findOneAndUpdate(query, update, { upsert: true });
   res.json(data);
 };
